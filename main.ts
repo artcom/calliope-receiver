fff31ca3fe6961dffe69ba753617e473838cf70e
elRadish <andreas.rettig@artcom.de>
2022-12-23 00:08:35 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ radio.onReceivedString(function (receivedString) {
     serial.writeLine(receivedString)
 })
 serial.setBaudRate(BaudRate.BaudRate9600)
+serial.setTxBufferSize(128)
+radio.setFrequencyBand(42)
 radio.setGroup(1)
 basic.forever(function () {
 	
